eb58ca9db68f1893e2507e10e828da6d32a08142
mintaekim <kmintae33@gmail.com>
2024-02-17 01:56:39 +0800

    import react bootstrap

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,10 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CreatePriorityForm from "./components/CreatePriorityForm";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import AddPriority from "./components/AddPriority";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
     <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+      <AddPriority show={modalShow} onHide={() => setModalShow(false)} />
       <div>
         <Routes>
           <Route path="/add" element={<CreatePriorityForm />} />
