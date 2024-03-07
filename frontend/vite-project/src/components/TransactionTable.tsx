ee07a4eae36235cecdfacce0cc1ce5eeac46a580
mintaekim <kmintae33@gmail.com>
2024-03-07 21:20:54 +0800

    implement pagination logic

--- frontend/vite-project/src/components/TransactionTable.tsx
+++ frontend/vite-project/src/components/TransactionTable.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import TransactionItem from "./TransactionItem";
 import Pagination from "react-bootstrap/Pagination";
 
 function TransactionTable(props: any) {
   const { selectedMonth, filterTransactionsByMonth, transactions } = props;
+  const itemsPerPage = 5;
+  const [currentPage, setCurrentPage] = useState(1);
 
-  let active = 2;
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Handle page change
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Generate pagination items
   let items = [];
-  for (let number = 1; number <= 5; number++) {
+  for (
+    let number = 1;
+    number <= Math.ceil(transactions?.length / itemsPerPage);
+    number++
+  ) {
     items.push(
-      <Pagination.Item key={number} active={number === active}>
+      <Pagination.Item
+        key={number}
+        active={number === currentPage}
+        onClick={() => handlePageChange(number)}
+      >
         {number}
       </Pagination.Item>
     );
@@ -23,20 +43,22 @@ function TransactionTable(props: any) {
       ) : (
         <>
           {selectedMonth !== null
-            ? filterTransactionsByMonth(transactions, selectedMonth).map(
-                (transaction: { _id: any }) => (
+            ? filterTransactionsByMonth(transactions, selectedMonth)
+                .slice(startIndex, endIndex)
+                .map((transaction: { _id: any }) => (
                   <TransactionItem
                     key={transaction._id}
                     transaction={transaction}
                   />
-                )
-              )
-            : transactions?.map((transaction: { _id: any }) => (
-                <TransactionItem
-                  key={transaction._id}
-                  transaction={transaction}
-                />
-              ))}
+                ))
+            : transactions
+                ?.slice(startIndex, endIndex)
+                .map((transaction: { _id: any }) => (
+                  <TransactionItem
+                    key={transaction._id}
+                    transaction={transaction}
+                  />
+                ))}
           <Pagination>{items}</Pagination>
         </>
       )}
